84923d832312301b5d4578e9ce9ad6bc0cc82afb
Mauricio Aizaga <mauricioaizaga@gmail.com>
2021-10-08 10:01:11 -0500

    Prepare DB from .env (not used yet)

--- etsy_shop_analyzer/settings.py
+++ etsy_shop_analyzer/settings.py
@@ -88,11 +88,20 @@ WSGI_APPLICATION = 'etsy_shop_analyzer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# Parse database connection url strings
+# like psql://user:pass@127.0.0.1:8458/db
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # read os.environ['DATABASE_URL'] and raises
+    # ImproperlyConfigured exception if not found
+    #
+    # The db() method is an alias for db_url().
+    # 'default': env.db(),
+
+    # # read os.environ['SQLITE_URL']
+    # 'extra': env.db_url(
+    #     'SQLITE_URL',
+    #     default='sqlite:////tmp/my-tmp-sqlite.db'
+    # )
 }
 
 
