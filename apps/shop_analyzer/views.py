e9062ff41dbf98d0ce8c8ecf4225db2aa26a8b1c
Mauricio Aizaga <mauricioaizaga@gmail.com>
2021-10-11 10:43:20 -0500

    show basic info in web

--- apps/shop_analyzer/views.py
+++ apps/shop_analyzer/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from apps.shop_analyzer.models import Shop
+
 
 def home(request):
+
+    shops_qs = Shop.objects.all()
+
     return render(request, 'shop_analyzer/home.html', locals())
