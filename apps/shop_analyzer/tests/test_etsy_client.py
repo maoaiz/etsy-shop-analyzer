1052a391f1ae4cff290242e2a104e59d97b097d3
Mauricio Aizaga <mauricioaizaga@gmail.com>
2021-10-11 09:13:15 -0500

    Testing all requests

--- apps/shop_analyzer/tests/test_etsy_client.py
+++ apps/shop_analyzer/tests/test_etsy_client.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from unittest.mock import patch
 
 from apps.shop_analyzer.integrations.etsy.client import EtsyClient
 
@@ -21,3 +22,22 @@ class TestEtsyClient(TestCase):
         assert self.client.base_url == _base_url
         assert self.client.verbose is False
         assert self.client.key_string == self.key_string
+
+    def test_perform_request_is_called_in_requests_all_method(self):
+        self.client
+
+    @patch.object(EtsyClient, 'perform_request')
+    def test_request_all_not_returns_offset(self, mock_perform_request):
+        """
+        Verify when method RestClient.perform_request not returns offset,
+        it returns 0 objects
+        """
+
+        response = {'count': 0, 'results': []}
+        mock_perform_request.return_value = 200, response
+
+        response = self.client._request_all('/some-endpoint')
+
+        response = list(response)
+
+        self.assertEqual(len(response), 0)
