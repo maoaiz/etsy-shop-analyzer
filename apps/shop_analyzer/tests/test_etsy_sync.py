805ad4d3682e9c55345622d57b0664a96327743d
Mauricio Aizaga <mauricioaizaga@gmail.com>
2021-10-11 09:50:35 -0500

    sync methdos called

---
+++ apps/shop_analyzer/tests/test_etsy_sync.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+from unittest.mock import patch
+
+from apps.shop_analyzer.integrations.etsy.client import EtsyClient
+from apps.shop_analyzer.integrations.etsy.sync import EtsySync
+
+
+class TestEtsyClient(TestCase):
+    '''Tests for all methods in EtsyClient class'''
+
+    def setUp(self):
+        self.key_string = '123456789acbdefghi'  # fake key string
+        self._verbose = False
+        self.sync = EtsySync(self.key_string, verbose=self._verbose)
+
+    def test_defaults(self):
+        '''Verify all params are defined correctly'''
+
+        self.assertIsInstance(self.sync._client, EtsyClient)
+
+    @patch.object(EtsySync, 'sync_shops')
+    @patch.object(EtsySync, 'sync_items_for_new_shops')
+    def test_sync_methods_are_called_called(self, mock_sync_items_for_new_shops, mock_sync_shops):
+        '''
+        EtsySync.sync must call two methods:
+            EtsySync.sync_shops
+            EtsySync.sync_items_for_new_shops
+        '''
+
+        self.sync.sync()
+
+        mock_sync_shops.assert_called_once()
+        mock_sync_items_for_new_shops.assert_called_once()
